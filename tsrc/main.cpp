d0a90d66178a5732a033e5965957da3af9361c27
Arttu Valo <arttu.valo@gmail.com>
2012-12-08 19:02:55 +0200

    apr-helper: Added install rules.
    Added install rules for apr-helper, test program and test data.

--- tsrc/main.cpp
+++ tsrc/main.cpp
@@ -27,9 +27,9 @@ int main( int argc, char* argv[] )
   
   if (argc < 2)
   {
-      std::cerr << "Usage: " << argv[ 0 ] << " <ini file>" << std::endl;
+      std::cerr << "Usage: " << argv[0] << " <ini file>" << std::endl;
       //return 1;
-      file_name = "../test.ini";
+      file_name = "../test_data/test.ini";
   }
   else
   {
